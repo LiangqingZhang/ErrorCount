7e65c3f964cd5833cde8c1eab55a9f30e86140f6
LiangqingZhang <104813952+LiangqingZhang@users.noreply.github.com>
2022-05-16 20:00:17 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 import pickle
 from model import Model
 import sys
+
+
+
 modeltype=int(sys.argv[1])
 print("modeltype: ",modeltype)
-
+path = 'parameters.npy'
 newres = pickle.load(open("data.pkl","rb"))
-model = Model(newres)
+model = Model(newres,path)
 res = model.forward(modeltype)
 pickle.dump(res,open("res.pkl","wb"))
